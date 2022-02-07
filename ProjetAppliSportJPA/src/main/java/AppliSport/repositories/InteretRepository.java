a88a1b404cdd19904561dba24e7a0794ced7da84
C'Hell <guevin.nathan@gmail.com>
2022-02-07 17:18:47 +0100

    Correction 2 ?

--- ProjetAppliSportJPA/src/main/java/AppliSport/repositories/InteretRepository.java
+++ ProjetAppliSportJPA/src/main/java/AppliSport/repositories/InteretRepository.java
@@ -12,10 +12,10 @@ import AppliSport.model.Interet;
 public interface InteretRepository extends JpaRepository<Interet, Long>{
 
 	
-	@Query("select i from Interet i left join fetch i.utilisateur i where i.id=:id")
-	List<Interet> findByIdUtilisateur(@Param("id") Long id);
+	//@Query("select i from Interet i left join fetch i.utilisateur i where i.id=:id")
+	//List<Interet> findByIdUtilisateur(@Param("id") Long id);
 	
-	@Query("select i from Interet i left join fetch i.sport i where i.id=:id")
-	List<Interet> findByIdSport(@Param("id") Long id);
+	//@Query("select i from Interet i left join fetch i.sport i where i.id=:id")
+	//List<Interet> findByIdSport(@Param("id") Long id);
 	
 }
