726d552c0937bc49b30bf3649ef9f9bf6301fe17
Shootang <besneville.clement@gmail.com>
2022-02-07 15:28:28 +0100

    Equipe/Evenement Repos

--- ProjetAppliSportJPA/src/main/java/AppliSport/repositories/EquipeRepository.java
+++ ProjetAppliSportJPA/src/main/java/AppliSport/repositories/EquipeRepository.java
@@ -1,9 +1,18 @@
 package AppliSport.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import AppliSport.model.Club;
 import AppliSport.model.Equipe;
 
-public interface EquipeRepository extends JpaRepository<Equipe, Long>{
+public interface EquipeRepository extends JpaRepository<Equipe, Long> {
+
+	List<Equipe> findByNom(String nom);
+
+	List<Equipe> findByIdOrNom(Long id, String nom);
+
+	List<Equipe> findByClub(Club club);
 
 }
--- ProjetAppliSportJPA/src/main/java/AppliSport/repositories/EvenementRepository.java
+++ ProjetAppliSportJPA/src/main/java/AppliSport/repositories/EvenementRepository.java
@@ -1,9 +1,23 @@
 package AppliSport.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import AppliSport.model.Club;
 import AppliSport.model.Evenement;
 
 public interface EvenementRepository extends JpaRepository<Evenement, Long>{
+
+	List<Evenement> findByNom(String nom);
+	
+	List<Evenement> findByIdOrNom(Long id, String nom);
+	
+	List<Evenement> findByDateDebut(LocalDate dateDebut);
+	
+	List<Evenement> findByDateFin(LocalDate dateFin);
+	
+	List<Evenement> findByClub(Club club);
 
 }
