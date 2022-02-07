64e8897adc2aa0ace5bd2e8e00e71a50557b36c1
Antoine Rodriguez <rodriguez.antoine083@gmail.com>
2022-02-07 15:37:37 +0100

    oops

--- ProjetAppliSportJPA/src/main/java/AppliSport/repositories/UtilisateurRepository.java
+++ ProjetAppliSportJPA/src/main/java/AppliSport/repositories/UtilisateurRepository.java
@@ -40,7 +40,7 @@ public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long>{
 	Optional<Utilisateur> findByProfilUtilisateur(Profil profilUtilisateur);
 	
 	@Query("select u from Utilisateur u left join fetch Profil p where p.id=:id")
-	Optional<Utilisateur> findByIdProfilUtilisateur(Long id);
+	Optional<Utilisateur> findByIdProfilUtilisateur(@Param("id") Long id);
 	
 	
 	
