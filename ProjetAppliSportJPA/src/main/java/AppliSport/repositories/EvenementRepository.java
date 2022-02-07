610c6537dedc5ccca540ff2738ad79e6b52b3f03
Shootang <besneville.clement@gmail.com>
2022-02-07 15:40:28 +0100

    Equipe/Evenement Repos V2

--- ProjetAppliSportJPA/src/main/java/AppliSport/repositories/EvenementRepository.java
+++ ProjetAppliSportJPA/src/main/java/AppliSport/repositories/EvenementRepository.java
@@ -2,22 +2,28 @@ package AppliSport.repositories;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import AppliSport.model.Club;
 import AppliSport.model.Evenement;
+import AppliSport.model.Profil;
 
 public interface EvenementRepository extends JpaRepository<Evenement, Long>{
-
-	List<Evenement> findByNom(String nom);
 	
+	// ===================  Attribut identifiant ======================== //
+	List<Evenement> findByNom(String nom);
 	List<Evenement> findByIdOrNom(Long id, String nom);
 	
+	// ===================  Attribut date ======================== //
 	List<Evenement> findByDateDebut(LocalDate dateDebut);
-	
 	List<Evenement> findByDateFin(LocalDate dateFin);
 	
-	List<Evenement> findByClub(Club club);
+	// ===================  Attribut club ======================== //
+	@Query("select e from Evenement e left join fetch e.club where e.id=:id")
+	Optional<Profil> findByIdClub(@Param("id") Long id);
 
 }
