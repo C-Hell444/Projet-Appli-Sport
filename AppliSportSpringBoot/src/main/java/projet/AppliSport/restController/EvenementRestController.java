42e16d2fdc3689d8d9174f5e4c6377b26fb4e690
Aginath <agi.selliah@hotmail.fr>
2022-02-18 14:27:52 +0000

    RestController equipe/even/Club

---
+++ AppliSportSpringBoot/src/main/java/projet/AppliSport/restController/EvenementRestController.java
@@ -0,0 +1,76 @@
+package projet.AppliSport.restController;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import projet.AppliSport.exception.EvenementException;
+import projet.AppliSport.model.Evenement;
+import projet.AppliSport.services.EvenementService;
+import projet.AppliSport.views.Views;
+@RestController
+@RequestMapping("/api/evenement")
+public class EvenementRestController {
+	
+	@Autowired
+	private EvenementService evenementService;
+
+	@GetMapping("")
+	@JsonView(Views.Common.class)
+	public List<Evenement> getAll() {
+		List<Evenement> list = evenementService.getAll();
+		System.out.println(list);
+		return list;
+	}
+
+
+
+	@GetMapping("/{id}")
+	@JsonView(Views.Common.class)
+	public Evenement getById(@PathVariable Long id) {
+		return evenementService.getById(id);
+	}
+
+
+
+	@PostMapping("")
+	@JsonView(Views.Common.class)
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Evenement create(@Valid @RequestBody Evenement evenement, BindingResult br) {
+		if (br.hasErrors()) {
+			throw new EvenementException();
+		}
+		return evenementService.createOrUpdate(evenement);
+	}
+
+	@PutMapping("/{id}")
+	@JsonView(Views.Common.class)
+	public Evenement update(@PathVariable Long id, @Valid @RequestBody Evenement evenement, BindingResult br) {
+		if (evenement.getId() == null || id != evenement.getId() || br.hasErrors()) {
+			throw new EvenementException();
+		}
+		return evenementService.createOrUpdate(evenement);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		evenementService.deleteById(id);
+	}
+
+}
