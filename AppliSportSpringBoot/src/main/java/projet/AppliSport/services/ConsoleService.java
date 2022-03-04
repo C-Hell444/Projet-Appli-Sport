b79f1274f4305e28e55befc0df362207a3f5d581
Aginath <agi.selliah@hotmail.fr>
2022-03-04 16:31:58 +0000

    Creation dobjet (J'ai laissé admin utilisateur ehhhhh)

--- AppliSportSpringBoot/src/main/java/projet/AppliSport/services/ConsoleService.java
+++ AppliSportSpringBoot/src/main/java/projet/AppliSport/services/ConsoleService.java
@@ -8,7 +8,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import projet.AppliSport.model.Admin;
+import projet.AppliSport.model.Caracteristique;
 import projet.AppliSport.model.Club;
+import projet.AppliSport.model.Equipe;
+import projet.AppliSport.model.Evenement;
+import projet.AppliSport.model.Interet;
+import projet.AppliSport.model.Profil;
+import projet.AppliSport.model.Sexe;
+import projet.AppliSport.model.Sport;
 import projet.AppliSport.model.Utilisateur;
 
 
@@ -29,7 +36,7 @@ public class ConsoleService implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		LOGGER.info("ça fonctionne");
-		//initDataBase();
+		initDataBase();
 		
 	}
 	
@@ -58,6 +65,67 @@ public class ConsoleService implements CommandLineRunner {
 		club.setClubNom("club");
 		club.setNumTel("0606060606");
 		customUserDetailsService.createOrUpdate(club);
+		
+		Caracteristique c2 = new Caracteristique();
+		c2.setAgilite(2);
+		c2.setCollectif(3);
+		c2.setCreativite(4);
+		c2.setDetente(0);
+		c2.setDetermination(0);
+		c2.setEndurance(0);
+		c2.setPatience(2);
+		c2.setPuissance(0);
+		c2.setVitesse(0);
+	
+		
+		
+		Sport football= new Sport();
+		football.setNom("Foot");
+		
+		Sport basketball= new Sport();
+		basketball.setNom("Basket");
+		
+		Evenement Fete= new Evenement();
+		Fete.setClub(club);
+		Fete.setDateDebut(null);
+		Fete.setDateFin(null);
+		Fete.setNom("Fete");
+		
+		Evenement Fete2= new Evenement();
+		Fete2.setClub(club);
+		Fete2.setDateDebut(null);
+		Fete2.setDateFin(null);
+		Fete2.setNom("Fete2");
+		
+		Profil bg= new Profil();
+		bg.setDateNaissance(null);
+		bg.setPoids(40);
+		bg.setSexe(Sexe.H);
+		bg.setTaille(2);
+		bg.setCaracteristique(c2);
+		
+		Profil bg2= new Profil();
+		bg2.setDateNaissance(null);
+		bg2.setPoids(60);
+		bg2.setSexe(Sexe.F);
+		bg2.setTaille(2);
+		bg2.setCaracteristique(c2);
+		
+		Equipe equipe = new Equipe();
+		equipe.setClub(club);
+		equipe.setNom("PSG");
+		
+		Equipe equipe2 = new Equipe();
+		equipe2.setClub(club);
+		equipe2.setNom("OM");
+		
+		Interet yolo = new Interet();
+		yolo.setSport(football);
+		yolo.setUtilisateur(utilisateur);
+		
+		Interet dormir = new Interet();
+		dormir.setSport(basketball);
+		dormir.setUtilisateur(utilisateur);
 		
 		
 	}
