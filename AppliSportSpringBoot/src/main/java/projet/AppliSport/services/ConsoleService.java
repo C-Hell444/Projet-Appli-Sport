4584b6d5cc95f446078d5cfc45c1e56d9705461d
C'Hell <guevin.nathan@gmail.com>
2022-03-04 16:32:51 +0100

    Comptes de base

--- AppliSportSpringBoot/src/main/java/projet/AppliSport/services/ConsoleService.java
+++ AppliSportSpringBoot/src/main/java/projet/AppliSport/services/ConsoleService.java
@@ -7,6 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 
 import org.springframework.stereotype.Service;
 
+import projet.AppliSport.model.Admin;
+import projet.AppliSport.model.Club;
+import projet.AppliSport.model.Utilisateur;
+
 
 
 
@@ -18,16 +22,46 @@ public class ConsoleService implements CommandLineRunner {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 	
+	
+	
 	private static final Logger LOGGER=LoggerFactory.getLogger(ConsoleService.class);
 	
 	@Override
 	public void run(String... args) throws Exception {
 		LOGGER.info("ça fonctionne");
+		initDataBase();
 		
 	}
 	
 
-	
+	private void initDataBase() {
+		
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.setIdentifiant("user");
+		utilisateur.setMdp("user");
+		utilisateur.setMail("user@google.com");
+		utilisateur.setNom("user");
+		utilisateur.setPrenom("user");
+		utilisateur.setNumTel("0606060606");
+		customUserDetailsService.createOrUpdate(utilisateur);
+		
+		Admin admin = new Admin();
+		admin.setIdentifiant("admin");
+		admin.setMdp("admin");
+		admin.setMail("admin@google.com");
+		customUserDetailsService.createOrUpdate(admin);
+		
+		Club club = new Club();
+		club.setIdentifiant("club");
+		club.setMdp("club");
+		club.setMail("club@gmail.com");
+		club.setClubNom("club");
+		club.setNumTel("0606060606");
+		customUserDetailsService.createOrUpdate(club);
+		
+		
+	}
+	      
 	
 	
 
