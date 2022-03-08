f15f1107a64b4b414b60e8e7c7b39222d5370d91
Sarra <saffar.sarra@gmail.com>
2022-03-08 09:58:16 +0100

    Ajout search mail

--- AppliSportSpringBoot/src/main/java/projet/AppliSport/model/CompteRepository.java
+++ AppliSportSpringBoot/src/main/java/projet/AppliSport/model/CompteRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CompteRepository extends JpaRepository<Compte, Long> {
 
 	Optional<Compte> findByIdentifiant(String identifiant);
+	Optional<Compte> findByMail(String mail);
 	
 }
