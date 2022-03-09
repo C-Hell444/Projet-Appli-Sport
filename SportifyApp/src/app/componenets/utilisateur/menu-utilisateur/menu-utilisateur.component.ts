453764579cb2de9a812ada68c7716a1678353da5
C'Hell <guevin.nathan@gmail.com>
2022-03-09 14:20:00 +0100

    Utilisateur Componenent

--- SportifyApp/src/app/componenets/utilisateur/menu-utilisateur/menu-utilisateur.component.ts
+++ SportifyApp/src/app/componenets/utilisateur/menu-utilisateur/menu-utilisateur.component.ts
@@ -1,15 +1,35 @@
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
+import { Compte } from './../../../model/compte';
+import { CompteService } from './../../../services/compte.service';
+import { Utilisateur } from 'src/app/model/utilisateur';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-utilisateur',
   templateUrl: './menu-utilisateur.component.html',
-  styleUrls: ['./menu-utilisateur.component.css']
+  styleUrls: ['./menu-utilisateur.component.css'],
 })
 export class MenuUtilisateurComponent implements OnInit {
+  compte: Compte = new Compte();
 
-  constructor() { }
+  utilisateurProfil: Utilisateur = new Utilisateur();
+
+  constructor(
+    private compteService: CompteService,
+    private utilisateurService: UtilisateurService
+  ) {}
 
   ngOnInit(): void {
+    this.compteService
+      .getType(localStorage.getItem('login')!)
+      .subscribe((result) => {
+        this.compte = result;
+        this.utilisateurService
+          .getByIdWithProfil(this.compte.id!)
+          .subscribe((ok) => {
+            this.utilisateurProfil = ok;
+          });
+      });
   }
-
 }
