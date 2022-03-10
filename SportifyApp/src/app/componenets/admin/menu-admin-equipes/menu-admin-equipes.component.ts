5fb70663a03d470111807d3e1be680715f08ebe2
Sarra <saffar.sarra@gmail.com>
2022-03-10 01:04:27 +0100

    quelque menu pour le compte admin

--- SportifyApp/src/app/componenets/admin/menu-admin-equipes/menu-admin-equipes.component.ts
+++ SportifyApp/src/app/componenets/admin/menu-admin-equipes/menu-admin-equipes.component.ts
@@ -10,9 +10,30 @@ import { EquipeService } from 'src/app/services/equipe.service';
 })
 export class MenuAdminEquipesComponent implements OnInit {
   equipesObservable!: Observable<Equipe[]>;
+  equipes: Equipe[] = [];
+  equipes2: Equipe[] = [];
+  equipes3: Equipe[] = [];
   constructor(private equipeService: EquipeService) {}
 
   ngOnInit(): void {
-    this.equipesObservable = this.equipeService.getAll();
+    this.equipeService.getAll().subscribe((ok) => {
+      this.equipes = ok;
+      for (let e1 of this.equipes) {
+        this.equipeService.getByIdWithClub(e1.id!).subscribe((r1) => {
+          this.equipes2.push(r1);
+        });
+
+        this.equipeService.getByIdWithUtilisateur(e1.id!).subscribe((r2) => {
+          console.log(r2);
+          this.equipes3.push(r2);
+        });
+      }
+      // for (let e2 of this.equipes) {
+      //   this.equipeService.getByIdWithUtilisateur(e2.id!).subscribe((r2) => {
+      //     console.log(r2);
+      //     this.equipes3.push(r2);
+      //   });
+      // }
+    });
   }
 }
